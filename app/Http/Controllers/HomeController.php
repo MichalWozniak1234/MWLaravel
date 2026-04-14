cd263005b673df09fa538dd74fc551a801ee3d18
MichalWozniak1234 <wozniak.michal.2004@gmail.com>
2026-04-14 19:11:05 +0200

    zad1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Mix;
-use Illuminate\Http\Request;
-use App\Services\InternalEventsService;
-
 class HomeController extends Controller
 {
     public function index() : mixed
     {
-         $eventService = new InternalEventsService();
-         $result = $eventService->getNewestEvents();
-
-        //pobieramy z warstwy model dane których potrzebujemy na widoku
-        return view("home.index",["events"=>$result]);
+        return view("home.index");
     }
 }
--- app/Models/InternalEvent.php
+++ app/Models/InternalEvent.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class InternalEvent extends Model
 {
@@ -13,4 +14,8 @@ class InternalEvent extends Model
     const CREATED_AT = "CreationDateTime";
     const UPDATED_AT = "EditDateTime";
 
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'InternalEventId', 'Id');
+    }
 }
